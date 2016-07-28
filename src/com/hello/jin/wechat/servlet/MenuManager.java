cdf5ec04a63b32b947cece0b7a3c22273fd77cf3
l00251692 <982679298@qq.com>
2016-07-28 23:07:05 +0800

    [HelloJin.class]修改自定义菜单项

--- src/com/hello/jin/wechat/servlet/MenuManager.java
+++ src/com/hello/jin/wechat/servlet/MenuManager.java
@@ -50,81 +50,42 @@ public class MenuManager {
 	 */
 	private static Menu getMenu() {
 		CommonButton btn11 = new CommonButton();
-		btn11.setName("天气预报");
+		btn11.setName("心日记");
 		btn11.setType("click");
 		btn11.setKey("11");
 
-		CommonButton btn12 = new CommonButton();
-		btn12.setName("公交查询");
-		btn12.setType("click");
-		btn12.setKey("12");
 
-		CommonButton btn13 = new CommonButton();
-		btn13.setName("周边搜索");
-		btn13.setType("click");
-		btn13.setKey("13");
-
-		CommonButton btn14 = new CommonButton();
-		btn14.setName("历史上的今天");
-		btn14.setType("click");
-		btn14.setKey("14");
-
-		CommonButton btn21 = new CommonButton();
-		btn21.setName("歌曲点播");
+		ViewButton btn21 = new ViewButton();
+		btn21.setName("我的清单");
 		btn21.setType("click");
-		btn21.setKey("21");
+		btn21.setUrl("http://www.baidu.com"); 
 
-		CommonButton btn22 = new CommonButton();
-		btn22.setName("经典游戏");
-		btn22.setType("click");
-		btn22.setKey("22");
 
-		CommonButton btn23 = new CommonButton();
-		btn23.setName("美女电台");
-		btn23.setType("click");
-		btn23.setKey("23");
-
-		CommonButton btn24 = new CommonButton();
-		btn24.setName("人脸识别");
-		btn24.setType("click");
-		btn24.setKey("24");
-
-		CommonButton btn25 = new CommonButton();
-		btn25.setName("聊天唠嗑");
-		btn25.setType("click");
-		btn25.setKey("25");
-
-		CommonButton btn31 = new CommonButton();
-		btn31.setName("Q友圈");
+		ViewButton btn31 = new ViewButton();
+		btn31.setName("求监督");
 		btn31.setType("click");
-		btn31.setKey("31");
-
-		CommonButton btn32 = new CommonButton();
-		btn32.setName("电影排行榜");
-		btn32.setType("click");
-		btn32.setKey("32");
-
-		CommonButton btn33 = new CommonButton();
-		btn33.setName("幽默笑话");
-		btn33.setType("click");
-		btn33.setKey("33");
+		btn31.setUrl("http://www.baidu.com"); 
 		
-		ViewButton btn34 = new ViewButton();  
-		btn34.setName("使用帮助");  
-		btn34.setType("view");  
-		btn34.setUrl("http://www.baidu.com");  
+		ViewButton btn32 = new ViewButton();
+		btn31.setName("抱大腿");
+		btn31.setType("click");
+		btn31.setUrl("http://www.baidu.com"); 
+
+		
+		ViewButton btn33 = new ViewButton();  
+		btn33.setName("广场");  
+		btn33.setType("view");  
+		btn33.setUrl("http://www.baidu.com");  
         
+		/**
 		ComplexButton mainBtn1 = new ComplexButton();
 		mainBtn1.setName("生活助手");
 		mainBtn1.setSub_button(new Button[] { btn11, btn12, btn13, btn14 });
-
-		ComplexButton mainBtn2 = new ComplexButton();
-		mainBtn2.setName("休闲驿站");
-		mainBtn2.setSub_button(new Button[] { btn21, btn22, btn23, btn24, btn25 });
+		*/
 
 		ComplexButton mainBtn3 = new ComplexButton();
-		mainBtn3.setName("更多体验");
-		mainBtn3.setSub_button(new Button[] { btn31, btn32, btn33 ,btn34});
+		mainBtn3.setName("发现");
+		mainBtn3.setSub_button(new Button[] { btn31, btn32, btn33});
 
 		/**
 		 * 这是公众号xiaoqrobot目前的菜单结构，每个一级菜单都有二级菜单项<br>
@@ -134,7 +95,7 @@ public class MenuManager {
 		 * menu.setButton(new Button[] { mainBtn1, mainBtn2, btn33 });
 		 */
 		Menu menu = new Menu();
-		menu.setButton(new Button[] { mainBtn1, mainBtn2, mainBtn3 });
+		menu.setButton(new Button[] { btn11, btn21, mainBtn3 });
 
 		return menu;
 	}
